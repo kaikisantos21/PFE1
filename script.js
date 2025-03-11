498fb8f0761d4b40f55d3bb3b7fe86d8f8dd26bf
kaikisantos21 <160151596+kaikisantos21@users.noreply.github.com>
2025-03-11 15:55:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let frutas = ["morango", "manga", "uva"];
+console.log(frutas.length);
+
+let numeros = [10, 20, 30, 40, 50];
+console.log(numero[2]);
+
+let cores = ["vermelho", "azul"];
+cores.push("verde")
+
+let animais = ["cachorro", "gato"];
+animais.unshift("papagaio");
+console.log(animais);
+
+let alunos = ["pedro", "bruno", "matheus"];
+alunos.pop();
+console.log(alunos);
+
