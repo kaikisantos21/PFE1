1c461c27d14cd0c0db24a8198ee69693e8f6203f
kaikisantos21 <160151596+kaikisantos21@users.noreply.github.com>
2025-03-11 15:00:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let contatos = [{
+  nome: "Alice Rodirgues", telefone: "9786 345 2689", email: "alice@gmail.com" },
+{ nome: "Barbara Silva", telefone: "4758 748 0375", email: "bsilva14@gmail.com" },
+{ nome: "Carlos Maia", telefone: "1954 289 2964", email: "maiac0rlos@gmail.com" }];
+
+
+contatos.push({
+    nome: "Maisie Haley",
+    telefone: "0913 531 3030",
+    email: "risus.Quisque@urna.ca"
+});
+
